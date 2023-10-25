cbefdf40b59979e395c54b8da510f88c336a6a52
Andressa Almeida <andressa.vdk93@gmail.com>
2023-10-25 17:04:58 -0300

    add gifs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aprendendo-ingles';
+  title = 'Aprendendo Inglês';
 
   public jogoEmAndamento: boolean = true;
   public tipoEncerramento: string | undefined = '' 
